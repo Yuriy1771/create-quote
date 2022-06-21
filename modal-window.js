d265ad8095ffcb6b2b204342e0d07a70350f4e61
Yuriy <dementev.yuriy@list.ru>
2022-06-21 18:44:29 +0300

    html верстка, css стили, js

--- modal-window.js
+++ modal-window.js
@@ -5,6 +5,7 @@ let saveQuoteBtn = document.querySelector('.saveQuoteBtn');
 let nameQuote = document.querySelector('.nameQuote');
 let inputForName = document.querySelector('.inputForName');
 let textareaForQuote = document.querySelector('.textareaForQuote');
+let quoteZone = document.querySelector('.quoteZone');
 
 createQuoteBtn.onclick = () => {
 	modalWindow.style.display = 'block';
@@ -21,6 +22,42 @@ modalWindow.onclick = (event) => {
 }
 
 saveQuoteBtn.onclick = () => {
-let saveUserName = inputForName.value;
-let saveTextarea = textareaForQuote.value;
+	let savedValueInputName = saveValueInputName();
+	let savedValueInputTextarea = saveValueInputTextarea();
+	closeModalWindow();
+	addingQuote(savedValueInputName, savedValueInputTextarea);
 }
+
+function saveValueInputName() {
+	let saveUserName = inputForName.value;
+	return saveUserName;
+}
+
+function saveValueInputTextarea() {
+	let saveTextarea = textareaForQuote.value;
+	return saveTextarea;
+}
+
+function closeModalWindow() {
+		modalWindow.style.display = 'none';
+}
+
+function addingQuote(name, quote) {
+	let divBlock = document.createElement('div');
+	divBlock.className = 'quoteBlock';
+	// let nameZ = document.createElement('p');
+	// nameZ.className = 'nameQuote';
+	// let quoteZ = document.createElement('p');
+	// quoteZ.className = 'quoteText';
+	// divBlock.innerHTML +=   '@' + name;
+	// divBlock.innerHTML +=    quote;
+	
+	let h3 = document.createElement('h3');
+	h3.innerHTML += '- ' + name;
+
+
+	quoteZone.insertAdjacentElement('beforeend', divBlock);
+	h3.insertAdjacentElement('beforeend', h3);
+	name.value = '';
+}
+    
